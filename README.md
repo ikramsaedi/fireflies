91bdde270718c5aa850aa40c81912155971e028c
Ikram Saedi <ikramsaedi@gmail.com>
2025-02-12 13:11:50 +1100

    Cleanup

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-To run this program locally: yarn dev.
+# Web Audio Visualisation of Ten Million Fireflies
 
-You will need vite and tsc installed.
+This is an audio visualiser of the song [Fireflies by Owl City](https://www.youtube.com/watch?v=psuRGfAaju4) made using the [web audio API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API). It draws fireflies to a HTML canvas that get faster as the music gets louder, and slower as the music is quieter.
 
-This currently only works on Chrome
+## Access the deployed site here
+
+NOTE: This currently only works on Chrome.
+[Deployed site](https://ikramsaedi.github.io/audio-visualisation/)
+
+## How to run this program locally
+
+1. Ensure you have `vite` and `tsc` installed by using `yarn`
+2. Run `yarn dev` and visit http://localhost:5173/audio-visualisation/
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import firefliesAudio from "./assets/owl_city-fireflies.mp3";
 // TODO be able to actually resize the canvas
 export const WIDTH = window.innerWidth - 500;
 export const HEIGHT = window.innerHeight;
-const numFireflies = 50;
+const numFireflies = 70;
 
 // These are just magic numbers that I found work well with this song
 const LOUD_BEAT_AMPLITUDE = 220;
@@ -14,9 +14,9 @@ const QUIET_BEAT_AMPLITUDE = 160;
 
 const audio = document.querySelector("#audio") as HTMLAudioElement;
 audio.src = firefliesAudio;
-audio.addEventListener("play", main);
+audio.addEventListener("play", onPlay);
 
-function main() {
+function onPlay() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
   if (!audio || !canvas) {
@@ -31,28 +31,32 @@ function main() {
   if (!audioCtx || !canvasCtx) {
     return;
   }
-  // This helps u create an analyser node
-  // Which gets u all the frequency data
+  // This helps you create an analyser node
+  // Which gets you all the frequency data
   const analyser = audioCtx.createAnalyser();
 
   // This gets the fireflies audio input as a mediastream input
   // It needs to be in the mediastream format so that the analyser node can analyse it
   const source = audioCtx.createMediaStreamSource(
+    // Has to be typed as any because captureStream as a type doesn't exist
+    // on HTML Audio Elements... even though it is a real method on that element..
     (audio as any).captureStream()
   );
-  // Now the analyser has a mediastream input to analyse so u can grab the frequency data later
+  // Now the analyser has a mediastream input to analyse so you can grab the frequency data later
   source.connect(analyser);
 
-  // TODO actually understand what this means
   // half of the size of the FFT (Fast Fourier Transform) window
   // The FFT algorithm converts audio data into freq (x) by amplitude (y) data
   // the whole frequency range has been divided into the binCount number of bins
+  // As a result there will be 1024 bins
   const bufferLength = analyser.frequencyBinCount;
 
-  // At this point, data array is a bunch of zeroes
+  // At this point, data array is a bunch of zeroes eg: [0,0,0,0,0,0...]
   // This is an unsigned 8 bit int array. It has to be like this because
   // the byte frequency data method only accepts unsigned 8 bit int arrays
   const dataArray = new Uint8Array(bufferLength);
+  // PRESENTER NOTE:
+  // console.debug("dataArray", dataArray);
 
   const fireflies = generateFireflies(canvasCtx, canvas.width, canvas.height);
   draw(canvasCtx, analyser, dataArray, bufferLength, fireflies);
@@ -65,6 +69,7 @@ function generateFireflies(
 ): Firefly[] {
   const fireflies = [];
   for (let i = 0; i < numFireflies; i++) {
+    // Set the initial position of each firefly in random spots of the canvas
     const xPos = randomiseNumInRange(100, width - 100);
     const yPos = randomiseNumInRange(100, height - 100);
 
@@ -86,24 +91,25 @@ function draw(
 
   drawBackground(canvasCtx);
 
-  // each item in the array represents the decibel value for a specific frequency.
   // This is where the data array is actually filled with values
-  // each bin takes up a byte (can have val between 0-255)
-  // this is a list of amplitudes which are indexed by frequency bin
+  // and is populated with an array of amplitudes (decibel value dB) which are indexed by frequency bin (Hz range)
+  // The order is that the lowest frequency bin is first and the highest frequency is last
   analyser.getByteFrequencyData(dataArray);
+  // PRESENTER NOTE:
+  // console.log("filled dataArray", dataArray);
 
   const halfFirefliesIndex = Math.floor(fireflies.length / 2);
   // For each decibel amplitude value in the data array that is higher than my arbitrary threshold of 170
   // redraw the fireflies. this way they will move with the music
   // The louder the music, the higher the amplitude (and also the more decibel values in a row that are higher than the threshold)
-  // Therefore the fireflies will move more quickly when the music is louder as they are redrawn more frequently
-
-  // going from lowest freq to highest freq
+  // Therefore the fireflies will move more quickly, and get more opaque when the music is louder as they are redrawn more frequently
   for (let i = 0; i < bufferLength; i++) {
+    // Loud half of fireflies
     if (dataArray[i] > LOUD_BEAT_AMPLITUDE) {
       for (let j = 0; j < halfFirefliesIndex; j++) {
         fireflies[j].update();
       }
+      // Quiet half of fireflies
     } else if (dataArray[i] > QUIET_BEAT_AMPLITUDE) {
       for (let j = halfFirefliesIndex; j < fireflies.length; j++) {
         fireflies[j].update();
